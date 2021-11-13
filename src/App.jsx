3dc8e6fac6b33a2784a28df6bfc7f3fc3c975260
jynam <njy1007@gmail.com>
2021-11-13 17:23:45 +0900

    🚧 컴포넌트 생성 및 기초 라우팅 설정 진행 중

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Footer from './component/common/Footer';
+// import Header from './component/common/Header';
+import Main from './pages/Main';
+import CreateQuestion from './pages/question/CreateQuestion';
+import Question from './pages/question/Question';
+import SearchQuestion from './pages/question/SearchQuestion';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <>
+        {/* <Header /> */}
+        <Routes>
+          <Route to="/main" element={<Main />} />
+          <Route to="/question" element={<Question />}>
+            <Route to="/create" element={<CreateQuestion />} />
+            <Route to="/search" element={<SearchQuestion />} />
+          </Route>
+        </Routes>
+        {/* <Footer /> */}
+      </>
+    </BrowserRouter>
+  );
 }
 
 export default App;

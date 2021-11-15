af47552ebb5b683a8b03eb6f3c361c7bf740f1b3
seojin kim <001106ksj@gmail.com>
2021-11-15 14:13:04 +0900

    :lipstick: 폰트 추가 / 글로벌 스타일 파일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ import Main from './pages/Main';
 import CreateQuestion from './pages/question/CreateQuestion';
 import Question from './pages/question/Question';
 import SearchQuestion from './pages/question/SearchQuestion';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 function App() {
   return (
     <BrowserRouter>
-      <>
-        <Header />
-        <Routes>
-          <Route path="main" element={<Main />} />
-          <Route path="question/*" element={<Question />}>
-            <Route path="create" element={<CreateQuestion />} />
-            <Route path="search" element={<SearchQuestion />} />
-            <Route path="" element={<SearchQuestion />} />
-          </Route>
-          <Route path="/" element={<Main />} />
-        </Routes>
-        <Footer />
-      </>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="main" element={<Main />} />
+        <Route path="question/*" element={<Question />}>
+          <Route path="create" element={<CreateQuestion />} />
+          <Route path="search" element={<SearchQuestion />} />
+          <Route path="" element={<SearchQuestion />} />
+        </Route>
+        <Route path="/" element={<Main />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }

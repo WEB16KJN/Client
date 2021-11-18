7b8ddbb35abede476d41232e5d2fe2a0cd0eb468
seojin kim <001106ksj@gmail.com>
2021-11-18 19:10:08 +0900

    :lipstick: 문의하기 검색 뷰 중 하단 테이블 구현

--- src/pages/question/SearchQuestion.jsx
+++ src/pages/question/SearchQuestion.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DateSelector from '../../component/question/search/DateSelector';
+import QuestionTable from '../../component/question/search/QuestionTable';
 
 export default function SearchQuestion() {
   return (
     <div>
       <DateSelector />
+      <QuestionTable />
     </div>
   );
 }

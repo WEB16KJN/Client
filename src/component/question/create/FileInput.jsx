029e97ce3f6b121779added6a02d457241eadf5d
jynam <njy1007@gmail.com>
2021-11-19 16:43:07 +0900

    🚧 첨부파일 버튼 컴포넌트 분리

--- src/component/question/create/FileInput.jsx
+++ src/component/question/create/FileInput.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import CreateInput from './CreateInput';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { colors } from '../../../styles/color';
 
 export default function FileInput({ addOrDelete }) {
   return (
     <StyledFileInput>
       <CreateInput />
-      <StyledButton index={0}>찾아보기</StyledButton>
-      <StyledButton index={1}>{addOrDelete}</StyledButton>
+      <StyledBlackButton>찾아보기</StyledBlackButton>
+      <StyledWhiteButton>{addOrDelete}</StyledWhiteButton>
     </StyledFileInput>
   );
 }
@@ -22,21 +22,22 @@ const StyledFileInput = styled.div`
   display: flex;
 `;
 
-const StyledButton = styled.button`
+const StyledBlackButton = styled.button`
   width: 159px;
   height: 45px;
   font-size: 13px;
   border-color: ${colors.black};
-  ${(props) =>
-    props.index === 0 &&
-    css`
-      background-color: ${colors.black};
-      color: ${colors.white};
-    `}
-  ${(props) =>
-    props.index === 1 &&
-    css`
-      background-color: ${colors.white};
-      color: ${colors.black};
-    `}
+  background-color: ${colors.black};
+  color: ${colors.white};
+  margin-left: 6px;
+`;
+
+const StyledWhiteButton = styled.button`
+  width: 159px;
+  height: 45px;
+  font-size: 13px;
+  border-color: ${colors.black};
+  background-color: ${colors.white};
+  color: ${colors.black};
+  margin-left: 6px;
 `;

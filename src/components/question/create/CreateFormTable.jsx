9f1f4fee02d2768c36e063bde24af531ee2aed06
jynam <njy1007@gmail.com>
2021-12-03 22:14:48 +0900

    💄 폼 내 인풋 placeholder 내용 및 스타일 설정

--- src/components/question/create/CreateFormTable.jsx
+++ src/components/question/create/CreateFormTable.jsx
@@ -17,7 +17,12 @@ export default function CreateFormTable({ handleQuestionInput, handleQuestionFil
         <StyledUserName>{userName}</StyledUserName>
       </TableCell>
       <TableCell label="제목">
-        <CreateInput type="text" handleQuestionInput={handleQuestionInput} value={'title'} />
+        <CreateInput
+          type="text"
+          handleQuestionInput={handleQuestionInput}
+          value={'title'}
+          placeholder="제목을 입력해 주세요."
+        />
       </TableCell>
       <TableCell label="비밀글 여부">
         <input id="isSecret" type="checkbox" onChange={(e) => handleQuestionInput(e, 'secret')} />
@@ -25,15 +30,28 @@ export default function CreateFormTable({ handleQuestionInput, handleQuestionFil
       </TableCell>
       <TableRowLayout>
         <TableCell label="성명">
-          <CreateInput type="text" handleQuestionInput={handleQuestionInput} value={'name'} />
+          <CreateInput
+            type="text"
+            handleQuestionInput={handleQuestionInput}
+            value={'name'}
+            placeholder="성명을 입력해 주세요."
+          />
         </TableCell>
         <TableCell label="핸드폰 번호">
-          <CreateInput handleQuestionInput={handleQuestionInput} value={'phone'} type="tel" />
+          <CreateInput
+            handleQuestionInput={handleQuestionInput}
+            value={'phone'}
+            type="tel"
+            placeholder="ex) 010-1234-1234"
+          />
           <div>* 자세한 설명을 위한 것으로, 기재하지 않으실 경우 원활한 상담이 어렵습니다. </div>
         </TableCell>
       </TableRowLayout>
       <TableCell label="문의 내용">
-        <StyledTextarea onChange={(e) => handleQuestionInput(e, 'content')} />
+        <StyledTextarea
+          onChange={(e) => handleQuestionInput(e, 'content')}
+          placeholder="문의하실 내용을 입력해 주세요."
+        />
       </TableCell>
       <TableCell label="첨부파일">
         <FileInputs handleQuestionFileInput={handleQuestionFileInput} />
@@ -56,4 +74,5 @@ const StyledTextarea = styled.textarea`
   height: 136px;
   resize: none;
   border: 1px solid ${colors.gray4};
+  padding: 20px 16px;
 `;
--- src/components/question/create/CreateInput.jsx
+++ src/components/question/create/CreateInput.jsx
@@ -3,14 +3,21 @@ import styled from 'styled-components';
 import { colors } from '../../../styles/color';
 import PropTypes from 'prop-types';
 
-export default function CreateInput({ value, handleQuestionInput, type }) {
-  return <StyledCreateInput type={type} onChange={(e) => handleQuestionInput(e, value)} />;
+export default function CreateInput({ value, handleQuestionInput, type, placeholder }) {
+  return (
+    <StyledCreateInput
+      type={type}
+      onChange={(e) => handleQuestionInput(e, value)}
+      placeholder={placeholder}
+    />
+  );
 }
 
 CreateInput.propTypes = {
   value: PropTypes.string,
   handleQuestionInput: PropTypes.func,
   type: PropTypes.string,
+  placeholder: PropTypes.string,
 };
 
 const StyledCreateInput = styled.input`
@@ -18,4 +25,5 @@ const StyledCreateInput = styled.input`
   width: 100%;
   border: 1px solid ${colors.gray4};
   box-sizing: border-box;
+  padding: 14px 16px;
 `;

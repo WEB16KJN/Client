1b4bfa091bc2b90ccd15206683fc2c1bcff035dd
jynam <njy1007@gmail.com>
2021-12-03 19:13:03 +0900

    ✨ 스케줄 GET 구현

--- src/components/question/Schedule.jsx
+++ src/components/question/Schedule.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ScheduleUnit from './ScheduleUnit';
 import { colors } from '../../styles/color';
+import API_URL from '../../api/common/constants';
+import requestHandler from '../../api/common/request';
 
 export default function Schedule() {
+  const [schedule, setSchedule] = useState({
+    all: '-',
+    planned: '-',
+    completed: '-',
+    canceled: '-',
+  });
+  useEffect(async () => {
+    const response = await requestHandler.get(API_URL.GET.USER_SCHEDULE);
+    const { json } = response;
+    setSchedule(json.data);
+  }, []);
+
   return (
     <StyledSchedule>
-      <ScheduleUnit scheduleContents="전체일정" scheduleCount="1" />
-      <ScheduleUnit scheduleContents="방문/상담 예정" scheduleCount="0" />
-      <ScheduleUnit scheduleContents="방문/상담 완료" scheduleCount="0" />
-      <ScheduleUnit scheduleContents="전체일정" scheduleCount="0" />
+      <ScheduleUnit scheduleContents="전체일정" scheduleCount={schedule.all} />
+      <ScheduleUnit scheduleContents="방문/상담 예정" scheduleCount={schedule.planned} />
+      <ScheduleUnit scheduleContents="방문/상담 완료" scheduleCount={schedule.completed} />
+      <ScheduleUnit scheduleContents="전체일정" scheduleCount={schedule.canceled} />
     </StyledSchedule>
   );
 }
